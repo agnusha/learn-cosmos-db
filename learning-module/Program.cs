126c76d000deb8a792e789ceb6f279a4e5f0bee7
Ahniya Staravoitava <Ahniya_Staravoitava@epam.com>
2020-11-15 13:02:22 +0300

    feat: init task

---
+++ learning-module/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
+namespace learning_module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
